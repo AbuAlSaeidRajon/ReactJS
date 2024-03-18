709f1247710fdca3bca51ed556ebd63950bcd637
Saeid Md <khanrajon5@gmail.com>
2024-03-18 13:51:26 +0200

    box added

--- demo/src/main.jsx
+++ demo/src/main.jsx
@@ -5,6 +5,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const Box = () => {
+  return (
+    <div>
+      <p>Name:</p>
+      <p>Title:</p>
+      <p>Age:</p>
+    </div>
+  )
+}
+
 //JS bigins (Arrow function).............
 const name = "Md Abu Al Saeid"
 const first_page = 2022;
@@ -15,6 +25,11 @@ const Demo = () => {
   <div>
     <h1>Hello! My name is {name}</h1>
     <p>I have {2024 - first_page} years of building websites experience.</p>
+    <Box />
+    <Box />
+    <Box />
+    <Box />
+    <Box />
   </div>
   );
 };
@@ -24,10 +39,10 @@ const Demo = () => {
     <h1>Hello! My name is {name}</h1>
     <p>I have {2024 - first_page} years of building websites experience.</p>
   </div>
-  );*/
+  ); */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Demo/>
-    </React.StrictMode>,
-)
+  </React.StrictMode>
+);
