947bdfbe194a6d0bbda687da599d41f1941efeed
Saeid Md <khanrajon5@gmail.com>
2024-03-18 12:53:01 +0200

    h1, p tag added

--- demo/src/main.jsx
+++ demo/src/main.jsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const name = "Md Abu Al Saeid"
+const first_page = 2022;
+
+const demo = (
+  <div>
+    <h1>Hello! My name is {name}</h1>
+    <p>I have {2024 - first_page} years of building websites experience.</p>
+  </div>
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode> {demo} </React.StrictMode>,
 )
